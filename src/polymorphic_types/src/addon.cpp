6a30f5579bc2c8aa62bbdf9c4afa0b359699cd0e
brandonhewer <bh532@bath.ac.uk>
2018-12-19 16:48:11 +0000

    Remove invalid code in addon.cpp

--- src/polymorphic_types/src/addon.cpp
+++ src/polymorphic_types/src/addon.cpp
@@ -5,8 +5,6 @@ namespace Types {
 
 napi_value init_all (napi_env env, napi_value exports) {
   napi_value print_fn;
-  napi_create_function(env, NULL, 0, print, NULL, &print_fn);
-  napi_set_named_property(env, exports, "print", print_fn);
   return exports;
 }
 
